3e6d424793169fa41da0a6af13b15f96f7691732
Jan-Christoph Kuester <jckuester@gmail.com>
2021-05-24 21:00:16 +0200

    Add default_tags attribute to AWS provider

--- pkg/provider/config.go
+++ pkg/provider/config.go
@@ -20,15 +20,18 @@ func config(name string) (cty.Value, string, error) {
 // awsProviderConfig returns a default configuration for the Terraform AWS Provider.
 func awsProviderConfig() cty.Value {
 	return cty.ObjectVal(map[string]cty.Value{
-		"profile":                     cty.StringVal(os.Getenv("AWS_PROFILE")),
-		"region":                      cty.StringVal(os.Getenv("AWS_DEFAULT_REGION")),
 		"access_key":                  cty.StringVal(os.Getenv("AWS_ACCESS_KEY_ID")),
 		"allowed_account_ids":         cty.UnknownVal(cty.DynamicPseudoType),
 		"assume_role":                 cty.UnknownVal(cty.DynamicPseudoType),
+		"default_tags":                cty.UnknownVal(cty.DynamicPseudoType),
 		"endpoints":                   cty.UnknownVal(cty.DynamicPseudoType),
 		"forbidden_account_ids":       cty.UnknownVal(cty.DynamicPseudoType),
+		"ignore_tag_prefixes":         cty.UnknownVal(cty.DynamicPseudoType),
+		"ignore_tags":                 cty.UnknownVal(cty.DynamicPseudoType),
 		"insecure":                    cty.UnknownVal(cty.DynamicPseudoType),
 		"max_retries":                 cty.UnknownVal(cty.DynamicPseudoType),
+		"profile":                     cty.StringVal(os.Getenv("AWS_PROFILE")),
+		"region":                      cty.StringVal(os.Getenv("AWS_DEFAULT_REGION")),
 		"s3_force_path_style":         cty.UnknownVal(cty.DynamicPseudoType),
 		"secret_key":                  cty.StringVal(os.Getenv("AWS_SECRET_ACCESS_KEY")),
 		"shared_credentials_file":     cty.StringVal(os.Getenv("AWS_SHARED_CREDENTIALS_FILE")),
@@ -38,7 +41,5 @@ func awsProviderConfig() cty.Value {
 		"skip_region_validation":      cty.UnknownVal(cty.DynamicPseudoType),
 		"skip_requesting_account_id":  cty.UnknownVal(cty.DynamicPseudoType),
 		"token":                       cty.StringVal(os.Getenv("AWS_SESSION_TOKEN")),
-		"ignore_tag_prefixes":         cty.UnknownVal(cty.DynamicPseudoType),
-		"ignore_tags":                 cty.UnknownVal(cty.DynamicPseudoType),
 	})
 }
